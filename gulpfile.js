1425f23b28e8da3d577f287efb7c7c191ce3e1ce
Sikandar <Sikandarshabbir11@gmail.com>
2018-12-23 11:33:22 +0500

    Css compression and concatination

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,24 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
 var livereload = require('gulp-livereload');
+var concat = require('gulp-concat');
+var minifyCss = require('gulp-minify-css');
+
+
+
 var SCRIPTS_PATH = 'public/scripts/**/*.js';
+var CSS_PATH = 'public/css/**/*.css';
+var DIST_PATH = 'public/dist';
 
 
 
 gulp.task('styles', function() {
 	console.log("My First Styling Gulp Task");
+	return gulp.src(['public/css/reset.css',CSS_PATH])
+	.pipe(concat('styles.css'))
+	.pipe(minifyCss())
+	.pipe(gulp.dest(DIST_PATH))
+	.pipe(livereload());
 });
 
 
@@ -16,7 +28,7 @@ gulp.task('scripts', function() {
 	console.log("My First  Scripts Gulp Task");
 	return gulp.src(SCRIPTS_PATH)
 	.pipe(uglify())
-	.pipe(gulp.dest('public/dist'))
+	.pipe(gulp.dest(DIST_PATH))
 	.pipe(livereload());
 });
 
@@ -34,4 +46,5 @@ gulp.task('watch', function () {
 	require('./server.js');
 	livereload.listen();
 	gulp.watch(SCRIPTS_PATH, gulp.series('scripts'));
+	gulp.watch(CSS_PATH, gulp.series('styles'));
 });
